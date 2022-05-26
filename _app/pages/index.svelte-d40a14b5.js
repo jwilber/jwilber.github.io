3baf07f3b28a4137bcbecb3f00a1b920041d5d28
Jared Wilber <wilberjw@amazon.com>
2022-05-26 14:59:07 -0700

    fix issue with sourcemaps

---
+++ _app/pages/index.svelte-d40a14b5.js
@@ -0,0 +1,31 @@
+import{S as Rt,i as Bt,s as Dt,e as a,t as n,k as ut,c as s,a as c,h as o,d as i,m as dt,b as t,K as O,g as Gt,G as e,n as ft}from"../chunks/index-89a54c6d.js";/* empty css                       */function Mt(jt){let f,S,d,h,j,V,z,k,ht,C,r,H,P,R,q,F,J,N,W,v,K,L,g,Q,T,w,U,X,m,Y,Z,I,$,tt,et,rt,at,st,lt,nt,u,ot,p,A,vt,it,b,E,gt,ct,_,y,mt;return{c(){f=a("main"),S=a("div"),d=a("div"),h=a("div"),j=a("h1"),V=n("Hey! I'm Jared"),z=ut(),k=a("img"),C=ut(),r=a("p"),H=a("br"),P=n(`
+				I'm a `),R=a("span"),q=n("developer/data scientist at AWS AI"),F=n(`, currently based in San
+				Diego, California \u{1F334}.
+				`),J=a("br"),N=a("br"),W=n(`Online, I'm the author of a library for
+				`),v=a("a"),K=n("drawing hand-drawn charts in the browser"),L=n(`
+				, create
+				`),g=a("a"),Q=n("interactive explanations of machine learning concepts for Amazon"),T=n(`
+				, maintain a few fun
+				`),w=a("a"),U=n("open datasets"),X=n(`, and write data-driven
+				`),m=a("a"),Y=n("articles about skateboarding"),Z=n(". (See "),I=a("a"),$=n("projects"),tt=n(` for more).
+				`),et=a("br"),rt=a("br"),at=n(`
+				Offline, I like to surf, skateboard, eat good food, and practice yoga (still can't sit cross-legged,
+				though).`),st=a("br"),lt=a("br"),nt=ut(),u=a("p"),ot=n(`Feel free to reach out:\xA0\xA0
+				`),p=a("a"),A=a("img"),it=n(`\xA0
+				`),b=a("a"),E=a("img"),ct=n(`\xA0
+				`),_=a("a"),y=a("img"),this.h()},l(D){f=s(D,"MAIN",{class:!0});var G=c(f);S=s(G,"DIV",{class:!0});var pt=c(S);d=s(pt,"DIV",{class:!0});var B=c(d);h=s(B,"DIV",{id:!0,class:!0});var M=c(h);j=s(M,"H1",{class:!0});var bt=c(j);V=o(bt,"Hey! I'm Jared"),bt.forEach(i),z=dt(M),k=s(M,"IMG",{src:!0,alt:!0,class:!0}),M.forEach(i),C=dt(B),r=s(B,"P",{class:!0});var l=c(r);H=s(l,"BR",{}),P=o(l,`
+				I'm a `),R=s(l,"SPAN",{class:!0});var _t=c(R);q=o(_t,"developer/data scientist at AWS AI"),_t.forEach(i),F=o(l,`, currently based in San
+				Diego, California \u{1F334}.
+				`),J=s(l,"BR",{}),N=s(l,"BR",{}),W=o(l,`Online, I'm the author of a library for
+				`),v=s(l,"A",{class:!0,target:!0,rel:!0,href:!0});var kt=c(v);K=o(kt,"drawing hand-drawn charts in the browser"),kt.forEach(i),L=o(l,`
+				, create
+				`),g=s(l,"A",{class:!0,target:!0,rel:!0,href:!0});var wt=c(g);Q=o(wt,"interactive explanations of machine learning concepts for Amazon"),wt.forEach(i),T=o(l,`
+				, maintain a few fun
+				`),w=s(l,"A",{class:!0,href:!0});var It=c(w);U=o(It,"open datasets"),It.forEach(i),X=o(l,`, and write data-driven
+				`),m=s(l,"A",{class:!0,target:!0,rel:!0,href:!0});var At=c(m);Y=o(At,"articles about skateboarding"),At.forEach(i),Z=o(l,". (See "),I=s(l,"A",{class:!0,href:!0});var Et=c(I);$=o(Et,"projects"),Et.forEach(i),tt=o(l,` for more).
+				`),et=s(l,"BR",{}),rt=s(l,"BR",{}),at=o(l,`
+				Offline, I like to surf, skateboard, eat good food, and practice yoga (still can't sit cross-legged,
+				though).`),st=s(l,"BR",{}),lt=s(l,"BR",{}),l.forEach(i),nt=dt(B),u=s(B,"P",{class:!0});var x=c(u);ot=o(x,`Feel free to reach out:\xA0\xA0
+				`),p=s(x,"A",{class:!0,target:!0,rel:!0,href:!0});var yt=c(p);A=s(yt,"IMG",{class:!0,src:!0,alt:!0}),yt.forEach(i),it=o(x,`\xA0
+				`),b=s(x,"A",{class:!0,target:!0,rel:!0,href:!0});var xt=c(b);E=s(xt,"IMG",{class:!0,src:!0,alt:!0}),xt.forEach(i),ct=o(x,`\xA0
+				`),_=s(x,"A",{class:!0,target:!0,rel:!0,href:!0});var St=c(_);y=s(St,"IMG",{class:!0,src:!0,alt:!0}),St.forEach(i),x.forEach(i),B.forEach(i),pt.forEach(i),G.forEach(i),this.h()},h(){t(j,"class","svelte-14l3s6r"),O(k.src,ht=Ot)||t(k,"src",ht),t(k,"alt","my face spinning in circles"),t(k,"class","face svelte-14l3s6r"),t(h,"id","intro"),t(h,"class","svelte-14l3s6r"),t(R,"class","bold"),t(v,"class","bold svelte-14l3s6r"),t(v,"target","_blank"),t(v,"rel","noopener noreferrer"),t(v,"href","https://github.com/jwilber/roughViz"),t(g,"class","bold svelte-14l3s6r"),t(g,"target","_blank"),t(g,"rel","noopener noreferrer"),t(g,"href","https://mlu-explain.github.io/"),t(w,"class","bold svelte-14l3s6r"),t(w,"href","./datasets"),t(m,"class","bold svelte-14l3s6r"),t(m,"target","_blank"),t(m,"rel","noopener noreferrer"),t(m,"href","https://pudding.cool/2018/06/skate-music/"),t(I,"class","bold svelte-14l3s6r"),t(I,"href","./projects"),t(r,"class","svelte-14l3s6r"),t(A,"class","icon svelte-14l3s6r"),O(A.src,vt="icons/icons8-twitter.svg")||t(A,"src",vt),t(A,"alt","link to twitter"),t(p,"class","icon-a svelte-14l3s6r"),t(p,"target","_blank"),t(p,"rel","noopener noreferrer"),t(p,"href","https://twitter.com/jdwlbr"),t(E,"class","icon svelte-14l3s6r"),O(E.src,gt="icons/icons8-github.svg")||t(E,"src",gt),t(E,"alt","link to github"),t(b,"class","icon-a svelte-14l3s6r"),t(b,"target","_blank"),t(b,"rel","noopener noreferrer"),t(b,"href","https://github.com/jwilber"),t(y,"class","icon svelte-14l3s6r"),O(y.src,mt="icons/icons8-linkedin.svg")||t(y,"src",mt),t(y,"alt","link to linkedin"),t(_,"class","icon-a svelte-14l3s6r"),t(_,"target","_blank"),t(_,"rel","noopener noreferrer"),t(_,"href","https://www.linkedin.com/in/jared-wilber/"),t(u,"class","icon-links svelte-14l3s6r"),t(d,"class","column-left column"),t(S,"class","content-wrapper"),t(f,"class","svelte-14l3s6r")},m(D,G){Gt(D,f,G),e(f,S),e(S,d),e(d,h),e(h,j),e(j,V),e(h,z),e(h,k),e(d,C),e(d,r),e(r,H),e(r,P),e(r,R),e(R,q),e(r,F),e(r,J),e(r,N),e(r,W),e(r,v),e(v,K),e(r,L),e(r,g),e(g,Q),e(r,T),e(r,w),e(w,U),e(r,X),e(r,m),e(m,Y),e(r,Z),e(r,I),e(I,$),e(r,tt),e(r,et),e(r,rt),e(r,at),e(r,st),e(r,lt),e(d,nt),e(d,u),e(u,ot),e(u,p),e(p,A),e(u,it),e(u,b),e(b,E),e(u,ct),e(u,_),e(_,y)},p:ft,i:ft,o:ft,d(D){D&&i(f)}}}let Ot="./images/me.jpg";class Ct extends Rt{constructor(f){super(),Bt(this,f,null,Mt,Dt,{})}}export{Ct as default};
